543979e08613a3f941c7b811c6e4ea370b526d71
riadh <farhatiered@gmail.com>
2019-10-04 10:09:19 +0100

    register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+// Load User model
+const User = require("../../modules/User");
 
 //@route Get api/users/test
 //@desc  Tests users route
@@ -7,6 +11,39 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({
     msg: "users works"
+  });
+});
+//@route Get api/users/register
+//@desc  register users route
+//@access public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //Rating
+        d: "mm" //Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log("erore save", err));
+        });
+      });
+    }
   });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
 
 const app = exporess();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const db = require("./config/keys").mongoURI;
 
 mongoose
